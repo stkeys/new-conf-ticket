b6e150e54cbd6d0d97e1de5f0e6f17f49d993dfc
Ola-olukotun Mary <maryomowumi76@gmail.com>
2025-09-02 00:57:25 +0100

    feat: more features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,19 @@ export default function Home() {
     backgroundImage: "url('./images/background-mobile.png')"
   }
   return (
-    <div style={bgColored} className="font-inconsolata bg-cover bg-center    w-full min-h-screen ">
+    <div style={bgColored} className=" bg-cover bg-center  min-h-screen flex flex-col items-center text-center px-4">
       <Image 
         src="/images/logo-full.svg" 
         alt="Logo" 
-        width={200} 
-        height={100} 
+        width={150} 
+        height={80} 
         className="mx-auto pt-10"
       />
+      <div className="md:w-[50%]">
+        <h1 className="text-2xl md:text-4xl text-[hsl(0,0%,100%)] font-bold py-6">Your Journey to Coding Conf 2025 Starts Here!</h1>
+        <p></p>
+      </div>
+      
     </div>
   );
 }
