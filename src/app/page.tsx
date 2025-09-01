8cef95296972e0eba322d4984834c5ee6ce35f19
Ola-olukotun Mary <maryomowumi76@gmail.com>
2025-09-02 00:13:22 +0100

    add images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-     <div className="font-inconsolata">
-
-     </div>
+    <div className="font-inconsolata bg-white min-h-screen text-black">
+      <h1>Coook my meal</h1>
+    </div>
   );
 }
