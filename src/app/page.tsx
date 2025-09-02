80d4cc35a1b7f84609b5a48cd9a10c0a498a5061
Ola-olukotun Mary <maryomowumi76@gmail.com>
2025-09-02 01:20:57 +0100

    feat: add tags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,10 +15,15 @@ export default function Home() {
         className="mx-auto pt-10"
       />
       <div className="md:w-[50%]">
-        <h1 className="text-2xl md:text-4xl text-[hsl(0,0%,100%)] font-bold py-6">Your Journey to Coding Conf 2025 Starts Here!</h1>
-        <p></p>
+        <h1 className="text-2xl md:text-4xl text-[hsl(0,0%,100%)] font-bold pt-6">Your Journey to Coding Conf 2025 Starts Here!</h1>
+        <p className="pt-2 text-[hsl(252,6%,83%)]">Secure your spot at next year's biggest coding conference</p>
       </div>
-      
+      <div className="flex items-start flex-col w-full md:w-[50%] pt-4">
+        <p className="text-[hsl(0,0%,100%)] ">Upload Avatar</p>
+        <div className="bg-[hsl(245, 19%, 35%)] h-40 w-full border border-dashed border-white">
+
+        </div>
+      </div>
     </div>
   );
 }
