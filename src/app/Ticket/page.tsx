9a101e19d0cc38743561835d415e33b7b25ec283
Ola-olukotun Mary <maryomowumi76@gmail.com>
2025-09-05 03:08:49 +0100

    add more styling in ticket page

--- src/app/Ticket/page.tsx
+++ src/app/Ticket/page.tsx
@@ -17,8 +17,8 @@ export default function Ticket() : React.ReactElement {
           className="mx-auto pt-10"
         />
       </div>
-      <div className="md:w-[50%] pt-6 mb-4">
-        <h1 className="text-2xl  md:text-5xl text-[hsl(0,0%,100%)]">
+      <div className="md:w-[50%] pt-6 mb-4 flex items-center flex-col justify-center">
+        <h1 className="text-2xl  md:text-5xl text-[hsl(0,0%,100%)] font-bold">
           Congrats, 
           <span 
           className="bg-gradient-to-r from-[hsl(7,88%,67%)] to-[hsl(252,6%,83%)] bg-clip-text text-transparent">
@@ -26,11 +26,9 @@ export default function Ticket() : React.ReactElement {
           </span> 
           Your ticket is ready
         </h1>
-        <p>
-          We ve emailed your ticket to 
-          <span className="text-[hsl(7,88%,67%)]">
-            jonatan@email.com
-          </span> 
+        <p className="md:w-[50%] pt-6">
+          We ve emailed your ticket to <span className="text-[hsl(7,88%,67%)]">
+            jonatan@email.com </span> 
           and will send updates in the run up to the event
         </p>
       </div>
