e884c61299f2dbeac83e6135e8bc989ebda5aa27
Ola-olukotun Mary <maryomowumi76@gmail.com>
2025-09-05 03:29:28 +0100

    add ticket pattern

--- src/app/Ticket/page.tsx
+++ src/app/Ticket/page.tsx
@@ -18,20 +18,40 @@ export default function Ticket() : React.ReactElement {
         />
       </div>
       <div className="md:w-[50%] pt-6 mb-4 flex items-center flex-col justify-center">
-        <h1 className="text-2xl  md:text-5xl text-[hsl(0,0%,100%)] font-bold.">
+        <h1 className="text-2xl  md:text-5xl text-[hsl(0,0%,100%)] font-bold">
           Congrats, 
           <span 
           className="bg-gradient-to-r from-[hsl(7,88%,67%)] to-[hsl(252,6%,83%)] bg-clip-text text-transparent">
             Jonathan <span className="bg-gradient-to-r from-[hsl(7,88%,67%)] to-[hsl(252,6%,83%)] bg-clip-text text-transparent">Kristof!</span>
           </span> 
-          Your ticket is ready
+          Your ticket is ready.
         </h1>
         <p className="md:w-[50%] pt-6">
           We ve emailed your ticket to <span className="text-[hsl(7,88%,67%)]">
             jonatan@email.com </span> 
           and will send updates in the run up to the event.
         </p>
+
+        <div className="relative">
+          <Image 
+            src="/images/pattern-ticket.svg" 
+            alt="Ticket" 
+            width={300} 
+            height={200} 
+            className="mx-auto pt-10 md:w-[400px] md:h-[300px]"
+          />
+          <div className="absolute top-[40%] left-10 transform -translate-x-1/2 -translate-y-1/2">
+            <Image 
+              src="/images/logo-mark.svg" 
+              alt="Ticket Full"
+              width={20 }
+              height={20 }
+              className="" 
+            />
+          </div>
+        </div>
       </div>
+
       
     </div>
  )
